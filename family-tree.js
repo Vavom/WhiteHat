169fc1f328b9732406949eedbdb5596e11bbdb6c
Vavom <nshamash123@gmail.com>
2020-09-23 16:09:54 +0100

    checking if parents age is higher than child

--- family-tree.js
+++ family-tree.js
@@ -1,7 +1,10 @@
-const sister = {name: 'Amy', age: 24 }
-const mum = {name: 'Mum', age: 52}
-const me = {name: 'Noah', age: 18, parents: [mum]}
-const dad = {name: 'Dad', age: 55}
+
+const mum = {name: 'Mum', age: 52, parents: []}
+const dad = {name: 'Dad', age: 55, parents: []}
+const sister = {name: 'Amy', age: 24, parents: [mum, dad] }
+const me = {name: 'Noah', age: 18, parents: [mum,dad]}
+
+
 
 
 module.exports = [me, sister, mum, dad]
--- family-tree.test.js
+++ family-tree.test.js
@@ -20,11 +20,16 @@ describe('Family Tree', function (){
         familyTree.map(a => expect(a.name).toBeDefined())
         familyTree.map(a => expect(a.name).not.toBeNull())
         familyTree.map(a => expect(a.name).not.toBe(Number))
-
-
     })
 
     test('All ages are above -1', function() {
         familyTree.map(a => expect(a.age).toBeGreaterThan(-1))
+    })
+
+    test('All parents are older than children', function() {
+        const me = familyTree[0]
+        const sister = familyTree[1]
+
+        expect(me.age).toBeLessThan(me.parents[0].age && me.parents[1].age)
     })
 })
